acc2540d10f3bf9e2bd3473fcaf8e0592ce87966
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-03-14 23:00:15 -0700

    updated searchbar UI

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { Graphs } from "./graphs";
 import { FoodInterface } from "../interfaces/food-interface";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLongArrowAltRight,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const SearchBar = () => {
   const [searchValue, setSearchValue] = useState<String>("");
@@ -28,30 +33,50 @@ export const SearchBar = () => {
   return (
     <div className="p-[30px]">
       {selection ? <Graphs data={selection} /> : null}
-      <div className="flex">
-        <input
-          type="text"
-          className="border flex-1 py-[3px] px-[5px]"
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-        <button
-          className="border px-[5px] hover:bg-gray-100"
-          onClick={searchDatabase}
-        >
-          submit
-        </button>
-      </div>
-      {isLoading ? <div>loading...</div> : null}
-      <div className="flex flex-col">
-        {searchResults.map((e) => (
+      <div>
+        <form className="flex border-2">
+          <div className="flex items-center">
+            <FontAwesomeIcon
+              className="text-gray-400 pl-[10px] absolute"
+              icon={faMagnifyingGlass}
+            ></FontAwesomeIcon>
+          </div>
+          <input
+            type="text"
+            className="flex-1 py-[3px] pl-[35px] pr-10px"
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="search..."
+          />
           <button
-            key={e.fdcId}
-            className="hover:bg-gray-100 text-left truncate"
-            onClick={() => setSelection(e)}
+            className="px-[10px] hover:bg-gray-100"
+            onClick={(e) => {
+              e.preventDefault();
+              searchDatabase();
+            }}
           >
-            {e.description}
+            <FontAwesomeIcon
+              className="text-gray-400"
+              icon={faLongArrowAltRight}
+            ></FontAwesomeIcon>
           </button>
-        ))}
+        </form>
+        {isLoading ? <div>loading...</div> : null}
+        {searchResults.length > 0 ? (
+          <div className="flex flex-col border-2 border-t-0">
+            {searchResults.map((e, i) => (
+              <button
+                key={e.fdcId}
+                className={
+                  "text-left truncate px-[10px] py-[2px] focus:z-10 focus:bg-amber-200 focus:outline-none hover:bg-amber-200" +
+                  (i % 2 === 0 ? " bg-gray-100 " : "")
+                }
+                onClick={() => setSelection(e)}
+              >
+                {e.description}
+              </button>
+            ))}
+          </div>
+        ) : null}
       </div>
     </div>
   );
