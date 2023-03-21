81352c621c99e28cb65635d12b270c05f0b84af3
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-03-20 20:09:35 -0700

    added footer

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SearchBar } from "./components/searchbar";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <div>
       <h1 className="text-[40px] text-left pt-[30px] px-[30px]">FoodStat</h1>
       <SearchBar />
+      <Footer />
     </div>
   );
 }
