843779f1ae298875c06802406f9c76c065543a80
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-03-22 19:00:48 -0700

    removed client files

--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <div className="bg-black">
-      <p className="text-white text-xs px-[30px] py-[2px]">
-        Copyright © 2023 Nicolas Matthew Lee
-      </p>
-    </div>
-  );
-};
